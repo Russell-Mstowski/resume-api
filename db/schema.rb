e0ce2878e0e41da3a6ad73453e009f729b31480e
Linda-Diamond-Robbins <lindadiamondrobbins@gmail.com>
2017-01-09 20:13:38 -0600

    create experience model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110005948) do
+ActiveRecord::Schema.define(version: 20170110014049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "job_title"
+    t.string   "company_name"
+    t.text     "details"
+    t.integer  "student_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
