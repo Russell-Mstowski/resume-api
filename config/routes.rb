41d8556bfbd97d8664b60d99bb3a2e8686645865
Chris Woodside <christopherwoodside@gmail.com>
2017-01-10 21:05:10 -0600

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   get '/experiences' => 'experiences#index'
   get '/experiences/:id' => 'experiences#show'
+  get '/api/v1/experiences'
 
   get '/allroads' => "resumes#index"
   get '/' => 'students#index'
-  
+
 end
