c2b03a6633a863de10f61a35f640da42bfccc681
Chris Woodside <christopherwoodside@gmail.com>
2017-01-10 21:24:23 -0600

    Add all missing routes and change existing nonsense routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get '/capstones' => 'capstones#index'
+  get '/capstones/:id' => 'capstones#show'
+
+  get '/educations' => 'educations#index'
+  get '/educations/:id' => 'educations#show'
+
   get '/experiences' => 'experiences#index'
   get '/experiences/:id' => 'experiences#show'
-  get '/api/v1/experiences'
 
-  get '/allroads' => "resumes#index"
   get '/' => 'students#index'
+  get '/students/:id' => 'students#show'
 
 end
