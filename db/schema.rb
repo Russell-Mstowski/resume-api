2329d1d034eebe45cdd5af48b351490ed06ce97d
Myles Woerner <myleswoerner@gmail.com>
2017-01-10 19:44:46 -0600

    fix issues with migration and schema files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110014816) do
+ActiveRecord::Schema.define(version: 20170110032539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,12 +25,12 @@ ActiveRecord::Schema.define(version: 20170110014816) do
   end
 
   create_table "educations", force: :cascade do |t|
-    t.string   "student_id"
     t.string   "start_date"
     t.string   "end_date"
     t.string   "degree"
     t.string   "university_name"
     t.text     "details"
+    t.integer  "student_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
